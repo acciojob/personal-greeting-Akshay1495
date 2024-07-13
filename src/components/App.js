55039a7b0b73b7e0821974cf2077736be5255442
Akshay <8009akshy@gmail.com>
2024-07-13 07:38:25 +0530

    correction

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
-
-import React,{useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  const[name,setName] = useState('');
+  const [name, setName] = useState("");
 
-  const handleChange =(event)=>{
-     setName(event.target.value);
+  const onChangeEvent = (event) => {
+    setName(`Hello ${event.target.value}!`);
   };
 
   return (
     <div>
-      <p>Enter Your name:</p>
-  <input 
-    type="text"
-    value={name}
-    onChange={handleChange}
-  />
-
-  {name && <p>Hello {name}!</p>}
+      {
+        <>
+          <input type="{text}" onChange={onChangeEvent} />
+          <p className="display">{name}</p>
+        </>
+      }
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
